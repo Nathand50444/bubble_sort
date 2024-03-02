5a18d45f14d362fcd75a92d8f8cb6721d04bc738
NathanD <nathand50444@gmail.com>
2024-03-02 20:37:45 +0000

    Bubble_ sort solution with further comments.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,9 +1,7 @@
-array = [3,8,10,17,2,1,22,7]
-
 def bubble_sort(array)
     swapped = true                                                  # 'Swapped' becomes true.
     while swapped do                                                # Whilst 'Swapped' is true, perform these actions.
-        swapped = false                                             # 'Swapped' becomes false.
+        swapped = false                                             # 'Swapped' becomes false. This loop will only end when all swaps are completed and 'swapped' is false throughout all iterations of the array.
         0.upto(array.size-2) do |i|                                 # From the index 0 value to the second-to-last value we will iterate through and perform the following actions.
             if array[i] > array[i+1]                                # If the selected 'i' value is larger than the 'i' +1 value...
                 array[i], array[i+1] = array[i+1], array[i]         # The 'i' and 'i'+1 values will be swapped, after which 'Swapped' becomes true again.
@@ -11,5 +9,10 @@ def bubble_sort(array)
             end
         end
     end
-    array
+    return array
+    
 end
+
+array = [3,8,10,17,2,1,22,7]
+
+return bubble_sort(array)
